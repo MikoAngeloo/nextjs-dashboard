3521bea893b09beb75967771620795459087f1c2
MikoAngelo <miko.coronado@betinasia.com>
2024-02-19 13:34:07 +0800

    some changes

--- app/ui/fonts.tsx
+++ app/ui/fonts.tsx
@@ -1,10 +1,6 @@
 // @/app/ui/fonts.ts
 
-// Example font list
-export const lusitana: string[] = [
-    'Arial',
-    'Helvetica',
-    'Times New Roman',
-    'Courier New',
-    // Add more fonts as needed
-];
+export const lusitana = {
+    className: 'your-class-name-here'
+  };
+  
